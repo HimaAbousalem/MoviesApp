1d1be922c95cca3dbeae5b92dfe9753e8bf56e1f
HimaAbousalem <hima.abousalem@gmail.com>
2016-04-17 18:43:18 +0200

    update

--- app/src/main/java/com/example/hima/moviesapp/DetailedActivityFragment.java
+++ app/src/main/java/com/example/hima/moviesapp/DetailedActivityFragment.java
@@ -79,7 +79,7 @@ public class DetailedActivityFragment extends Fragment implements View.OnClickLi
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
              @Override
              public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(data.get(position).getKey())));
+                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(data.get(position-1).getKey())));
              }
          });
 
